09f0755dca48337610488b72e239d31ee70339f3
mainlib990 <mainlib990@gmail.com>
2023-06-20 17:02:49 +0900

    feat(post): Controller 추가

--- backend/src/main/java/com/_42kh/backend/web/PostController.java
+++ backend/src/main/java/com/_42kh/backend/web/PostController.java
@@ -1,11 +1,35 @@
 package com._42kh.backend.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com._42kh.backend.service.post.PostService;
+import com._42kh.backend.web.post.dto.PostRequest;
+import com._42kh.backend.web.post.dto.PostResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/posts")
 public class PostController {
 
-    //TODO: 구현하세요.
+    private final PostService postService;
+
+    @PostMapping
+    public Long save(@RequestBody PostRequest postRequest) {
+        return postService.save(postRequest);
+    }
+
+    @GetMapping("/{id}")
+    public PostResponse findById(@PathVariable("id") Long id) {
+        return postService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Long update(@PathVariable("id") Long id, @RequestBody PostRequest postRequest) {
+        return postService.update(id, postRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public Long delete(@PathVariable("id") Long id) {
+        return postService.delete(id);
+    }
 }
