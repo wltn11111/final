a49f797b567a1e37aaae14ff299954f94146673f
park cho hye <chohye@172.20.nate.com>
2023-06-20 17:06:23 +0900

    view: Join Component 추가

---
+++ frontend/src/Components/Body/Join/Join.js
@@ -0,0 +1,32 @@
+import style from './Join.module.css';
+import { Link } from 'react-router-dom';
+
+
+
+export default function () {
+
+  return (
+
+            <div className={style.join_container}>
+            <div>
+            <span className={`${style.join_title } d-flex justify-content-center`}>회원가입</span>
+            <hr></hr>
+          <Link to = "/joinbyemail">
+          <p className={style.join_mail}>
+            <i class={`${style.mail_icon} ri-mail-send-line`}></i>
+          이메일 인증으로 가입
+          </p>
+          </Link>
+          <Link to = "/joinbyphone">
+          <p className={style.join_phone}>
+          <i class={`${style.phone_icon} ri-smartphone-line`}></i>
+     핸드폰 인증으로 가입
+          </p>
+          </Link>
+
+          </div>
+          </div>
+
+  )
+
+}
