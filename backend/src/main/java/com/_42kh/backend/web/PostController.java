df012ddf2c20a9a22337c43446761b4b6e7aca01
mainlib990 <mainlib990@gmail.com>
2023-06-20 16:32:58 +0900

    feat(post): PostsController에서 PostController로 수정

--- backend/src/main/java/com/_42kh/backend/web/PostController.java
+++ backend/src/main/java/com/_42kh/backend/web/PostController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/posts")
-public class PostsController {
+public class PostController {
 
     //TODO: 구현하세요.
 }
