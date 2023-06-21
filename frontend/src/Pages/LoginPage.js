b197a91848d7800a69f3d0d6a467578b59922bda
park cho hye <chohye@172.20.nate.com>
2023-06-21 11:35:37 +0900

    view: LoginPage 추가

---
+++ frontend/src/Pages/LoginPage.js
@@ -0,0 +1,16 @@
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import Login from "../Components/Body/Login/Login";
+
+export default function () {
+
+  return (
+    <>
+    <Header></Header>
+    <Login></Login>
+    <Footer></Footer>
+    </>
+
+  )
+
+}
