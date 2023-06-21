4c977f5b1ccdce7d7acf998163fbbb0e5fade715
park cho hye <chohye@172.20.nate.com>
2023-06-21 11:36:34 +0900

    view: App.css , App.js 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
+import 'remixicon/fonts/remixicon.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
+import { Route, Router, Routes } from 'react-router-dom';
+import BoardWritePage from './Pages/BoardWritePage';
+import BoardViewPage from './Pages/BoardViewPage';
+import BoardListPage from './Pages/BoardListPage';
+import Index from './Pages/Index';
+import Search from './Pages/Search';
+import LoginPage from './Pages/LoginPage';
+import JoinPage from './Pages/JoinPage';
+import MyPage from './Pages/MyPage';
+
 
 function App() {
+  const [posts,setPosts] = useState([{id : 0, title : "1",
+  contents : "test", author : "3ddddddd", category : "4", createdDate : "5", modifiedDate : "6" },])
+
+  const [user,setUser] = useState({id : "" , password : ""})
+
+//   axios.get('/posts').then((resp)=>{
+//    setPosts(resp.data)
+//  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-flud">
+    <Routes>
+    <Route path = "/" element = {<Index></Index>}/>
+    <Route path = "/posts" element = {<BoardListPage posts = {posts}></ BoardListPage>}/>
+    <Route path='/newpost' element = {<BoardWritePage></BoardWritePage>}/>
+    <Route path='/post' element = {<BoardViewPage></BoardViewPage>}/>
+    <Route path = "/search" element = {<Search></Search>}></Route>
+    <Route path = "/login" element = {<LoginPage></LoginPage>}></Route>
+    <Route path = "/join" element = {<JoinPage></JoinPage>}></Route>
+    <Route path = "/mypage" element = {<MyPage></MyPage>}>
+
+    </Route>
+
+    </Routes>
     </div>
+
   );
 }
 
