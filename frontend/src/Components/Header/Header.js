587587b857ca994f7c1b764afcf850da642b7a1b
park cho hye <chohye@172.20.nate.com>
2023-06-21 11:24:53 +0900

    view: Header Component 추가

---
+++ frontend/src/Components/Header/Header.js
@@ -0,0 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navi from './Navi/Navi';
+import {LogOutIcon,LoginIcon} from './Icon/Icon';
+
+export default function ({setHeaderView,setSearchView}) {
+
+  return (
+
+    <div className="row sticky-top header" style={{margin : "0px", backgroundColor : "white"}}>
+    <div className="col-6 col-md-2"></div>
+    <div className="d-none d-md-block col-md-8">
+    <Navi> </Navi>
+    </div>
+    <div className="col-6 col-sm-none col-md-2" id = "icon_area">
+    {/* <LogOutIcon setHeaderView ={setHeaderView} setSearchView = {setSearchView}></LogOutIcon> */}
+     <LoginIcon setHeaderView ={setHeaderView} setSearchView = {setSearchView}></LoginIcon>
+    </div>
+  </div>
+
+  )
+}
