718bc8f061a891f6b186d385a48b516dbdbfea56
park cho hye <chohye@172.20.nate.com>
2023-06-21 11:34:19 +0900

    view: BoardViewPage 추가

---
+++ frontend/src/Pages/BoardViewPage.js
@@ -0,0 +1,15 @@
+import ViewForm from "../Components/Body/Board/ViewForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+
+export default function () {
+
+  return (
+    <>
+    <Header></Header>
+    <ViewForm></ViewForm>
+    <Footer></Footer>
+    </>
+  )
+}
