e57e183a44a7a00642509f8284d9c906d84af3d5
park cho hye <chohye@172.20.nate.com>
2023-06-21 11:14:03 +0900

    view: Index 추가

---
+++ frontend/src/Pages/Index.js
@@ -0,0 +1,19 @@
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import Category from "../Components/Body/Category/Category";
+import PostButton from "../Components/PostButton/PostButton";
+
+export default function () {
+
+  return (
+    <>
+    <Header></Header>
+    <Category></Category>
+    <Footer></Footer>
+    <PostButton></PostButton>
+
+    </>
+
+  )
+
+}
