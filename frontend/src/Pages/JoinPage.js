b470987f3795d3a9f4fcf18bdce250f504315699
park cho hye <chohye@172.20.nate.com>
2023-06-21 11:35:10 +0900

    view: JoinPage 추가

---
+++ frontend/src/Pages/JoinPage.js
@@ -0,0 +1,15 @@
+import Header from "../Components/Header/Header"
+import Footer from "../Components/Footer/Footer"
+import Join from "../Components/Body/Join/Join"
+
+
+export default function () {
+  return (
+    <>
+    <Header></Header>
+    <Join></Join>
+    <Footer></Footer>
+    </>
+
+  )
+}
