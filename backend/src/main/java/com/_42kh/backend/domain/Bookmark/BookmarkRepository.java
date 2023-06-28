a92bfdd6d1425f38347617db8054d66bde48ad49
mainlib990 <43126937+mainlib990@users.noreply.github.com>
2023-06-28 15:56:31 +0900

    Revert "feat(bookmark): 기능 구현 - 임시 (#29)" (#34)
    This reverts commit e9b0c4d8110d91b7107a7da4fb1de90e5f2f231b.

--- backend/src/main/java/com/_42kh/backend/domain/Bookmark/BookmarkRepository.java
+++
@@ -1,7 +0,0 @@
-package com._42kh.backend.domain.Bookmark;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookmarkRepository extends JpaRepository<Bookmark, Long>{
-    
-}
