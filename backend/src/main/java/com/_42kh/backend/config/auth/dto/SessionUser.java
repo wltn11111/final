a37ddb197e0687c001d620d6f5726566fcca612e
mainlib990 <43126937+mainlib990@users.noreply.github.com>
2023-06-29 16:17:27 +0900

    feat(auth): SessionUser에 CreatedDate 추가 (#45)

--- backend/src/main/java/com/_42kh/backend/config/auth/dto/SessionUser.java
+++ backend/src/main/java/com/_42kh/backend/config/auth/dto/SessionUser.java
@@ -4,6 +4,7 @@ import com._42kh.backend.domain.user.User;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 public class SessionUser implements Serializable {
@@ -11,10 +12,12 @@ public class SessionUser implements Serializable {
     private final String name;
     private final String email;
     private final String picture;
+    private final LocalDateTime createdDate;
 
     public SessionUser(User user) {
         this.name = user.getName();
         this.email = user.getEmail();
         this.picture = user.getPicture();
+        this.createdDate = user.getCreatedDate();
     }
 }
