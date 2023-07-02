37ddd0b96b7579432beef091f7203bd8d037c799
mainlib990 <43126937+mainlib990@users.noreply.github.com>
2023-07-02 16:21:30 +0900

    fix(bookmark): URL 주소 복수형으로 변경 (#68)

--- backend/src/main/java/com/_42kh/backend/web/bookmark/BookmarkController.java
+++ backend/src/main/java/com/_42kh/backend/web/bookmark/BookmarkController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/v1/bookmark")
+@RequestMapping("/api/v1/bookmarks")
 public class BookmarkController {
 
     private final BookmarkService bookmarkService;
