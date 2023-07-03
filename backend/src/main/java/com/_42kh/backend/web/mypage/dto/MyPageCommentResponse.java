4b290c38c2e7ba72f8e2f979c4ac9d230ca5446c
mainlib990 <43126937+mainlib990@users.noreply.github.com>
2023-07-03 20:00:27 +0900

    feat(mypage): 반환 값 변경 (#84)
    * feat(mypage): Posts 반환 값 변경
    
    * feat(mypage): Comments 반환 값 변경

--- backend/src/main/java/com/_42kh/backend/web/mypage/dto/MyPageCommentResponse.java
+++ backend/src/main/java/com/_42kh/backend/web/mypage/dto/MyPageCommentResponse.java
@@ -3,20 +3,16 @@ package com._42kh.backend.web.mypage.dto;
 import com._42kh.backend.domain.comment.Comment;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class MyPageCommentResponse {
 
     private final Long id;
+    private final Long postId;
     private final String contents;
-    private final LocalDateTime createdDate;
-    private final LocalDateTime modifiedDate;
 
     public MyPageCommentResponse(Comment comment) {
         id = comment.getId();
+        postId = comment.getPost().getId();
         contents = comment.getContents();
-        createdDate = comment.getCreatedDate();
-        modifiedDate = comment.getModifiedDate();
     }
 }
